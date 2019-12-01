8e954a37040fbb8b68bdc36b0364df43a52445e2
Rebkova <rebkova@gmail.com>
2019-12-01 13:18:00 +0100

    Deleted solution, starting from scratch

--- 1_Fuel/Program.cs
+++ 1_Fuel/Program.cs
@@ -13,73 +13,28 @@ namespace _1_Fuel
             //Debug example:
             //Part1(new List<int>{ 1, 23, 12312 }.GetEnumerator());
 
-            Part1(inputProvider);
+            //Part1(inputProvider);
+            Part1(new List<int> { 12, 661, 213 }.GetEnumerator());
 
-            inputProvider.Reset();
+            //inputProvider.Reset();
 
-            Part2(inputProvider);
+            //Part2(inputProvider);
         }
 
         private static void Part1(IEnumerator<int> inputProvider)
         {
-            int totalMass = 0;
-            int totalFuelMass = 0;
-            bool requiresFuelForfuel = false;
-
             while (inputProvider.MoveNext())
             {
-                int mass = inputProvider.Current;
+                int x = inputProvider.Current;
 
-                var fuelMass = GetFuelMassForModule(requiresFuelForfuel, mass);
-                totalFuelMass += fuelMass;
-                totalMass += mass + fuelMass;
+                Console.WriteLine(x);
             }
-
-            Console.WriteLine($"Total fuel Required: {totalFuelMass}");
         }
 
         private static void Part2(IEnumerator<int> inputProvider)
         {
-            int totalMass = 0;
-            int totalFuelMass = 0;
-            bool requiresFuelForfuel = true;
 
-            while (inputProvider.MoveNext())
-            {
-                int mass = inputProvider.Current;
-
-                var fuelMass = GetFuelMassForModule(requiresFuelForfuel, mass);
-                totalFuelMass += fuelMass;
-                totalMass += mass + fuelMass;
-            }
-
-            Console.WriteLine($"Total mass Required: {totalMass}, of which {totalFuelMass} is Fuel");
         }
 
-        private static int GetFuelMassForModule(bool requiresFuelForfuel, int mass)
-        {
-            int fuelMass = GetFuelRequired(mass);
-            int totalFuelMass = fuelMass;
-
-            if (requiresFuelForfuel)
-            {
-                while (true)
-                {
-                    int addFuelMass = GetFuelRequired(fuelMass);
-
-                    if (addFuelMass <= 0) break;
-
-                    totalFuelMass += addFuelMass;
-                    fuelMass = addFuelMass;
-                }
-            }
-
-            return totalFuelMass;
-        }
-
-        private static int GetFuelRequired(int mass)
-        {
-            return (int)(Math.Floor(mass / 3.0) - 2);
-        }
     }
 }
