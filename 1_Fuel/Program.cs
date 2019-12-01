2dd6cdd388fdea143684075cabd132e31ff9daa8
David Božjak <bozjak@gmail.com>
2019-12-01 07:48:15 +0100

    Added Day1Part2, refactored

--- 1_Fuel/Program.cs
+++ 1_Fuel/Program.cs
@@ -5,20 +5,50 @@ namespace _1_Fuel
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var inputProvider = new InputProvider<int>("Input.txt", int.TryParse);
+            using var inputProvider = new InputProvider<int>("Input.txt", int.TryParse);
 
             int totalMass = 0;
+            int totalFuelMass = 0;
+            bool requiresFuelForfuel = true;
 
             while (inputProvider.MoveNext())
             {
                 int mass = inputProvider.Current;
-
-                totalMass += (int)(Math.Floor(mass / 3.0) - 2);
+                
+                var fuelMass = GetFuelMassForModule(requiresFuelForfuel, mass);
+                totalFuelMass += fuelMass;
+                totalMass += mass + fuelMass;
             }
 
-            Console.WriteLine($"Total mass Required: {totalMass}");
+            Console.WriteLine($"Total mass Required: {totalMass}, of which {totalFuelMass} is Fuel");
+        }
+
+        private static int GetFuelMassForModule(bool requiresFuelForfuel, int mass)
+        {
+            int fuelMass = GetFuelRequired(mass);
+            int totalFuelMass = fuelMass;
+
+            if (requiresFuelForfuel)
+            {
+                while (true)
+                {
+                    int addFuelMass = GetFuelRequired(fuelMass);
+
+                    if (addFuelMass <= 0) break;
+
+                    totalFuelMass += addFuelMass;
+                    fuelMass = addFuelMass;
+                }
+            }
+
+            return totalFuelMass;
+        }
+
+        private static int GetFuelRequired(int mass)
+        {
+            return (int)(Math.Floor(mass / 3.0) - 2);
         }
     }
 }
